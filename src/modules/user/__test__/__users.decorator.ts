4bb9596eb45ead759832a7444e323dcccde02751
Farshid <farenjir@gmail.com>
2023-08-26 23:45:31 +0330

    app_interceptor_refactor_filters

--- src/modules/user/__test__/__users.decorator.ts
+++
@@ -1,7 +0,0 @@
-// import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-
-// export const CurrentUser = createParamDecorator((_data: never, context: ExecutionContext) => {
-// 	const { currentUser } = context.switchToHttp().getRequest();
-// 	// return
-// 	return currentUser;
-// });
