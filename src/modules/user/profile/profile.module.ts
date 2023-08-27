2ae4dfe9243d03156091708990b35193fb007233
Farshid <farenjir@gmail.com>
2023-08-27 22:33:24 +0330

    user_module_refactor

--- src/modules/user/profile/profile.module.ts
+++ src/modules/user/profile/profile.module.ts
@@ -3,13 +3,11 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { CommonModule } from "global/common.module";
 
-import { UsersModule } from "../user.module";
-
 import { Profile } from "./profile.entity";
 import { ProfileService } from "./profile.service";
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Profile]), UsersModule, CommonModule],
+	imports: [TypeOrmModule.forFeature([Profile]), CommonModule],
 	providers: [ProfileService],
 	exports: [ProfileService],
 })
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,20 +3,16 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { CommonModule } from "global/common.module";
 
-import { Profile } from "./profile/profile.entity";
-import { ProfileService } from "./profile/profile.service";
-
-import { UserKYC } from "./kyc/kyc.entity";
-import { UserKycService } from "./kyc/kyc.service";
-
 import { Users } from "./user.entity";
 import { UsersService } from "./user.service";
 import { UsersController } from "./users.controller";
+import { ProfileModule } from "./profile/profile.module";
+import { UserKycModule } from "./kyc/kyc.module";
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Users, Profile, UserKYC]), CommonModule],
+	imports: [TypeOrmModule.forFeature([Users]), ProfileModule, UserKycModule, CommonModule],
 	controllers: [UsersController],
-	providers: [UsersService, ProfileService, UserKycService],
+	providers: [UsersService],
 	exports: [UsersService],
 })
 export class UsersModule {}
