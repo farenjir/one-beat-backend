d6b8edb2f98c4ce717478b5c5e7a1298a2e7230e
Farshid <farenjir@gmail.com>
2023-09-29 20:09:10 +0330

    env.production

--- src/global/serialize.decorator.ts
+++ src/global/serialize.decorator.ts
@@ -1,9 +1,6 @@
 import { SetMetadata } from "@nestjs/common";
-
-interface ClassConstructor<T = any> {
-	new (...args: any[]): T;
-}
+import { IClassConstructor } from "app/app.interceptor";
 
 export const SerializeKey = "SerializeKey";
 
-export const Serialize = (dto: ClassConstructor, exclude?: boolean) => SetMetadata(SerializeKey, { dto, exclude });
+export const Serialize = (dto: IClassConstructor, exclude?: boolean) => SetMetadata(SerializeKey, { dto, exclude });
