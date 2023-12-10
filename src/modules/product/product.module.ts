9d3c5413e7ce1e5a5b791e6fd97d3001b722a990
Farshid Enjili <f.enjili@fwutech.com>
2023-12-10 16:14:14 +0330

    add_find_product_service

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { GlobalModule } from "global/global.module";
 
+import { UsersModule } from "modules/user/user.module";
+
 import { Products } from "./product.entity";
 import { ProductsController } from "./products.controller";
 import { ProductsService } from "./product.service";
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Products]), GlobalModule],
+	imports: [TypeOrmModule.forFeature([Products]), GlobalModule, UsersModule],
 	controllers: [ProductsController],
 	providers: [ProductsService],
 })
